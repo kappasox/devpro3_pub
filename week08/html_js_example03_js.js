c1f79607f1fbf0837c944ffb540138daee8976e4
kappasox <takemoto.development@gmail.com>
2025-06-01 14:28:16 +0900

    week08のファイル（jpeg含んでいる～）

---
+++ week08/html_js_example03_js.js
@@ -0,0 +1,3 @@
+let x1 = 10
+document.write("Hello World(JavaScript part!!)__")
+document.write(x1)
---
+++ week08/html_js_example05a_js.js
@@ -0,0 +1,3 @@
+let x1 = 10
+document.write("Hello World(JavaScript part!!) 05A__")
+document.write(x1)
